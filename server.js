d57cda2a97af67fab408e899562a6073b3925b9b
nickaldwin <nicklemoncito98@gmail.com>
2023-06-04 05:54:54 +0000

    adding codes on server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = ecpress();
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const methodOverride = require("method-override");
+const flash = require("express-flash");
+const logger = require("morgan");
+const connectDB = require("./config/database");
+const mainRoutes = require("./routes/main");
+const postRoutes = require("./routes/posts");
+
+//use .env file in conmfig folder
+require("dotenv").config({path:"./config/.env"});
+
+
+
