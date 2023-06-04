6ce3b7e428b0dd5e1195543499b4050a469ab96e
nickaldwin <nicklemoncito98@gmail.com>
2023-06-04 06:35:50 +0000

    saving before brownOut

--- server.js
+++ server.js
@@ -14,5 +14,63 @@ const postRoutes = require("./routes/posts");
 //use .env file in conmfig folder
 require("dotenv").config({path:"./config/.env"});
 
+// Passport config
+require("./config/passport")(passport);
+
+//connect to database
+connectDB();
+
+//using ejs for views
+app.set("view engine", "ejs");
+
+//Body Parsing
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+//Logging
+app.use(logger("dev"));
+
+//use forms for put / delete
+app.use(methodOverride("_method"));
+
+//steup sessions  - sessions stored MongoDB
+app.use(
+    session({
+        secret: "Keyboard cat",
+        resave: false,
+        saveUninitialized: false,
+        store: new MongoStore({ mongooseConnection: mongoose.Connection});
+    })
+);
+
+//passport middleware
+app,use(passport.initialize);
+app.use(passport.session());
+
+//use flash
+app.use(flash());
+
+//setup routes for which the server Is listening
+app.use("/", mainRoutes);
+app.use("/post", postRoutes);
+
+//server runnig
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is running, you better catch it!");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
